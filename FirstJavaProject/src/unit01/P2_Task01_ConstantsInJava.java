8dba8dbd989a565a725a59368a7c9858b1bafb2f
anany <anany@LAPTOP-39BPG504>
2022-04-16 11:59:11 +0530

    nextcommit!

---
+++ FirstJavaProject/src/unit01/P2_Task01_ConstantsInJava.java
@@ -0,0 +1,11 @@
+package unit01;
+
+public class P2_Task01_ConstantsInJava {
+	static final float pi = 3.14f; // global constant
+	
+	public static void main(String[] args) {
+		final int r = 10; // local constatnt
+        float area = pi * r * r;
+        System.out.println(area);
+	}
+}
