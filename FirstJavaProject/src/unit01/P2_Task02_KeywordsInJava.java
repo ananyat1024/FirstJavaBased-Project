00ae925fbf7acdf828a7d0eace71214963fff8b3
anany <anany@LAPTOP-39BPG504>
2022-04-16 12:15:46 +0530

    next commit!

---
+++ FirstJavaProject/src/unit01/P2_Task02_KeywordsInJava.java
@@ -0,0 +1,24 @@
+package unit01;
+//import java.util.*;
+
+public class P2_Task02_KeywordsInJava {
+	float float =2;
+    // Syntax error on token "float", invalid VariableDeclaratorId
+     float();// Syntax error on tokens "float", identifier expected after this token
+}
+
+Static void float()
+{   // Syntax error on tokens "float", identifier expected
+    System.out.println("hello");
+}
+
+abc static = new abc();
+//Syntax error on token "static", invalid VariableDeclaratorId
+}
+class abc
+{
+int print()
+{
+    System.out.println("hello");
+}
+}
